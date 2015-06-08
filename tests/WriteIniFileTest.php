79d576b9a36b3eb41ff5ba346a29bea107b85c87
magicalex <magicalex14000@gmail.com>
2015-06-08 16:20:09 +0200

    adding unit test update & create

--- tests/WriteIniFileTest.php
+++ tests/WriteIniFileTest.php
@@ -4,38 +4,68 @@ use \WriteIniFile\WriteIniFile;
 
 class WriteIniFileTest extends \PHPUnit_Framework_TestCase
 {
-    public function testWrite()
-    {
-        $var = [
-            'section 1' => [
-                'foo' => 'string',
-                'bool_true' => true,
-                'bool_false' => false,
-                'int_true' => 1,
-                'int_false' => 0,
-                'int' => 10,
-                'float' => 10.3,
-                'foo_array' => [
-                    'string',
-                    10.3,
-                    true,
-                    false
-                ],
+    private $var = [
+        'section 1' => [
+            'foo' => 'string',
+            'bool_true' => true,
+            'bool_false' => false,
+            'int_true' => 1,
+            'int_false' => 0,
+            'int' => 10,
+            'float' => 10.3,
+            'foo_array' => [
+                'string',
+                10.3,
+                true,
+                false
             ],
-            'section 2' => [
-                'foo' => 'string',
-                'var' => 'string string',
-                'bool_true_string' => 'true',
-                'int_true_string' => '1',
-                'int_false_string' => '0',
-                'float_string' => '10.5L'
-            ]
-        ];
+        ],
+        'section 2' => [
+            'foo' => 'string',
+            'var' => 'string string',
+            'bool_true_string' => 'true',
+            'int_true_string' => '1',
+            'int_false_string' => '0',
+            'float_string' => '10.5L'
+        ]
+    ];
 
-        //$a = new WriteIniFile('../file_ini/test1.ini');
-        //$a->create($var);
-        //$a->write();
+    public function testCreate()
+    {
+        $actual = 'tests/file_ini/testCreate2.ini';
+        $expected = 'tests/file_ini/testCreate1.ini';
+        $a = new WriteIniFile($actual);
+        $a->create($this->var);
+        $a->write();
 
-        $this->assertFileEquals('tests/file_ini/test1.ini', 'tests/file_ini/test2.ini');
+        $this->assertFileEquals($expected, $actual);
+    }
+
+    public function testUpdate()
+    {
+        $actual = 'tests/file_ini/testUpdate2.ini';
+        $expected = 'tests/file_ini/testUpdate1.ini';
+        $b = new WriteIniFile($actual);
+        $b->create($this->var);
+        $b->write();
+        $b->update([
+            'section 1' => ['foo' => 'bar', 'int' => 100]
+        ]);
+        $b->write();
+
+        $this->assertFileEquals($expected, $actual);
+    }
+
+
+    public function testRm()
+    {
+    }
+
+    public function testErase()
+    {
+    }
+
+    public function testAdd()
+    {
     }
 }
